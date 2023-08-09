123c838e54bbcb7803ab478babf679c2e6f05850
Vitor Hugo Magnus Oliveira <vitorh.magnus.oliveira@gmail.com>
2023-08-09 18:17:26 -0300

    insert method and constructor

---
+++ hashtable_tags.cpp
@@ -0,0 +1,39 @@
+#include "hashtable_tags.h"
+
+Hash_tags::Hash_tags(int n){
+    len = n;
+    table.resize(n);
+}
+
+
+int Hash_tags::load(string filename){
+
+}
+void Hash_tags::insert(string tag, unsigned int sofifa_id){
+    int key = f_hash(tag);
+    Tag* current = table[key];
+
+    if(current == NULL) {
+        Tag* t_new = new Tag;
+        t_new->ids.push_back(sofifa_id);
+        t_new->tag = tag;
+        table[key] = t_new;
+        return;
+    }
+
+    while(current->tail != NULL && current->tag != tag)
+        current = current->tail;
+
+    if(current->tag != tag){
+        Tag* t_new = new Tag;
+        t_new->ids.push_back(sofifa_id);
+        t_new->tag = tag;
+        current->tail = t_new;
+    } else {
+        current->ids.push_back(sofifa_id);
+    }
+}
+
+Tag* Hash_tags::query(string tag){
+
+}
